840e37cbecc43620017fa1c66362f3a06a451d80
Adner Scarpelini <adner.scarpelini@gmail.com>
2025-05-14 10:26:06 -0300

    Configure Swagger for API documentation and UI

--- ags-todo-api/Controllers/TaskController.cs
+++ ags-todo-api/Controllers/TaskController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ags_todo_api.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ags_todo_api.Controllers
 {
@@ -7,6 +8,17 @@ namespace ags_todo_api.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Obtém uma tarefa específica pelo seu ID.
+        /// </summary>
+        /// <param name="id">O ID da tarefa a ser obtida.</param>
+        /// <returns>A tarefa encontrada ou NotFound se não existir.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskModel>> GetTask(int id)
+        {
+            return new TaskModel();
         }
     }
 }
--- ags-todo-api/Program.cs
+++ ags-todo-api/Program.cs
@@ -1,5 +1,6 @@
 using ags_todo_api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,29 @@ builder.Services.AddDbContext<TodoDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+//Adner: Configuração do SwaggerGen para gerar o documento swagger.json
+builder.Services.AddSwaggerGen(options =>
+{
+    //Indico que o Sweagger vai ler os comentários XML que eu colocar nos meus métodos do controller
+    var xmlFilename = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "AGS ToDo API",
+        Description = "A simple To-Do List API built with .NET Core and C#.",
+        Contact = new OpenApiContact
+        {
+            Name = "Adner Giovanni Scarpelini",
+            Email = "contato@adnerscarpelini.com.br",
+            Url = new Uri("https://adnerscarpelini.com.br/")
+        },
+    });
+
+});
 
 var app = builder.Build();
 
@@ -22,7 +45,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => // Middleware para servir a UI do Swagger
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "AGS ToDo API v1");
+    });
 }
 
 app.UseHttpsRedirection();
